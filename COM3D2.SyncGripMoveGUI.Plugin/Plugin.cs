992e03b5247eaf7aad5714a2fe2e8871479230c0
InoryS <101243352+InoryS@users.noreply.github.com>
2024-07-16 20:16:31 +0800

    1.0.4

--- COM3D2.SyncGripMoveGUI.Plugin/Plugin.cs
+++ COM3D2.SyncGripMoveGUI.Plugin/Plugin.cs
@@ -4,41 +4,64 @@ using BepInEx;
 using HarmonyLib;
 using BepInEx.Logging;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.VR;
+using CM3D2.GripMovePlugin.Plugin;
 
 namespace COM3D2.SyncGripMoveGUI.Plugin
 {
-    [BepInPlugin("com.inorys.syncgripmovegui", "COM3D2.SyncGripMoveGUI.Plugin", "1.0.2")]
+    [BepInPlugin("com.inorys.syncgripmovegui", "COM3D2.SyncGripMoveGUI.Plugin", "1.0.4")]
     public class SyncGripMoveGUI : BaseUnityPlugin
     {
         public static new ManualLogSource Logger;
         private static MethodInfo _toggleGUIVisibleMethod;
 
+        // Subscribe to scene loaded event to update the cached scene index
+        static SyncGripMoveGUI()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private static void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        {
+            // SceneIndex 9 is SceneTitle
+            if (scene.buildIndex == 9)
+            {
+                ExecuteStartFunctionality();
+            }
+        }
+        
+        
         private void Awake()
         {
             Logger = base.Logger;
             Logger.LogInfo("COM3D2.SyncGripMoveGUI.Plugin Loaded");
-
+            
             //Use Harmony to hook OvrTablet's SetVisible method
             Harmony harmony = new Harmony("com.inorys.syncgripmovegui");
             harmony.PatchAll();
         }
 
-        private void Start()
+        private static void ExecuteStartFunctionality()
         {
             // Get reflection information of GUIQuad.switchVisibility method
-            Type guiQuadType = Type.GetType("CM3D2.GripMovePlugin.Plugin.GUIQuad, CM3D2.GripMovePlugin.Plugin");
+            Type guiQuadType = AccessTools.TypeByName("CM3D2.GripMovePlugin.Plugin.GUIQuad");
             if (guiQuadType != null)
             {
-                _toggleGUIVisibleMethod = guiQuadType.GetMethod("switchVisibility", BindingFlags.Public | BindingFlags.Static);
+                Logger.LogInfo("Find type CM3D2.GripMovePlugin.Plugin.GUIQuad success.");
+                _toggleGUIVisibleMethod = guiQuadType.GetMethod("switchVisibility", BindingFlags.NonPublic | BindingFlags.Instance);
                 if (_toggleGUIVisibleMethod == null)
                 {
-                    Logger.LogError("switchVisibility method not found in GUIQuad.");
+                    Logger.LogError("CM3D2.GripMovePlugin.Plugin.GUIQuad.switchVisibility method not found in GUIQuad.");
+                }
+                else
+                {
+                    Logger.LogInfo("Find CM3D2.GripMovePlugin.Plugin.GUIQuad.switchVisibility method success.");
                 }
             }
             else
             {
-                Logger.LogError("GUIQuad type not found.");
+                Logger.LogError("CM3D2.GripMovePlugin.Plugin.GUIQuad type not found.");
             }
         }
 
