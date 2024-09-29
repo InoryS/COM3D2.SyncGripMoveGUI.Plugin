304a6c342cda7cbb2d05881055ea279b1d4dfb2f
InoryS <101243352+InoryS@users.noreply.github.com>
2024-09-29 15:16:40 +0800

    1.0.5.0 redo all

--- COM3D2.SyncGripMoveGUI.Plugin/Plugin.cs
+++ COM3D2.SyncGripMoveGUI.Plugin/Plugin.cs
@@ -1,102 +1,95 @@
 ﻿using System;
-using System.Reflection;
 using BepInEx;
 using HarmonyLib;
-using BepInEx.Logging;
+using System.Reflection;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.VR;
-using CM3D2.GripMovePlugin.Plugin;
+using System.Linq;
+using System.Globalization;
 
 namespace COM3D2.SyncGripMoveGUI.Plugin
 {
-    [BepInPlugin("com.inorys.syncgripmovegui", "COM3D2.SyncGripMoveGUI.Plugin", "1.0.4.1")]
+    [BepInPlugin("COM3D2.SyncGripMoveGUI.Plugin", "COM3D2.SyncGripMoveGUI.Plugin", "1.0.5.0")]
     public class SyncGripMoveGUI : BaseUnityPlugin
     {
-        public static new ManualLogSource Logger;
-        private static MethodInfo _toggleGUIVisibleMethod;
-        private static bool isVRMode;
-
-        // Subscribe to scene loaded event to update the cached scene index
-        static SyncGripMoveGUI()
+        void Awake()
         {
-            SceneManager.sceneLoaded += OnSceneLoaded;
-        }
-
-        private static void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
-        {
-            // SceneIndex 9 is SceneTitle
-            if (scene.buildIndex == 9)
-            {
-                ExecuteStartFunctionality();
-            }
-        }
-
-
-        private void Awake()
-        {
-            Logger = base.Logger;
-            isVRMode = Environment.CommandLine.ToLower().Contains("/vr");
-
-            if (!isVRMode)
+            if (!Environment.CommandLine.ToLower().Contains("/vr"))
             {
                 Debug.Log("Is NOT VR Mode. Shutdown COM3D2.SyncGripMoveGUI.Plugin.");
                 return;
             }
 
-            Logger.LogInfo("COM3D2.SyncGripMoveGUI.Plugin Loaded");
-
-            //Use Harmony to hook OvrTablet's SetVisible method
-            Harmony harmony = new Harmony("com.inorys.syncgripmovegui");
+            // 创建 Harmony 实例并应用补丁
+            var harmony = new Harmony("COM3D2.SyncGripMoveGUI.Plugin");
             harmony.PatchAll();
         }
+    }
 
-        private static void ExecuteStartFunctionality()
+    [HarmonyPatch]
+    public class OvrCamera_ShowUI_Patch
+    {
+        // 指定要补丁的方法为 OvrCamera 类的 ShowUI(bool f_bShow) 方法
+        static MethodBase TargetMethod()
         {
-            if (!isVRMode) return;
-            // Get reflection information of GUIQuad.switchVisibility method
-            Type guiQuadType = AccessTools.TypeByName("CM3D2.GripMovePlugin.Plugin.GUIQuad");
-            if (guiQuadType != null)
+            var ovrCameraType = typeof(OvrCamera);
+            return ovrCameraType.GetMethod("ShowUI",
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        }
+
+        // 在 ShowUI 方法执行后调用
+        public static void Postfix(bool f_bShow)
+        {
+            try
             {
-                Logger.LogInfo("Find type CM3D2.GripMovePlugin.Plugin.GUIQuad success.");
-                _toggleGUIVisibleMethod =
-                    guiQuadType.GetMethod("switchVisibility", BindingFlags.NonPublic | BindingFlags.Instance);
-                if (_toggleGUIVisibleMethod == null)
+                // 获取所有已加载的程序集
+                var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+                // 找到 GripMovePlugin 所在的程序集
+                var gripMoveAssembly =
+                    assemblies.FirstOrDefault(a => a.GetType("CM3D2.GripMovePlugin.Plugin.GripMovePlugin") != null);
+
+                if (gripMoveAssembly != null)
                 {
-                    Logger.LogError(
-                        "CM3D2.GripMovePlugin.Plugin.GUIQuad.switchVisibility method not found in GUIQuad.");
+                    // 通过反射获取 IMGUIQuad 类型
+                    var imguiQuadType = gripMoveAssembly.GetType("CM3D2.GripMovePlugin.Plugin.IMGUIQuad");
+
+                    if (imguiQuadType != null)
+                    {
+                        // 获取 IsVisble 属性
+                        var isVisbleProperty = imguiQuadType.GetProperty("IsVisble",
+                            BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+                        if (isVisbleProperty != null)
+                        {
+                            // 设置 IsVisble 属性的值，使其与游戏 UI 的可见性同步
+                            isVisbleProperty.SetValue(
+                                obj: null,
+                                value: f_bShow,
+                                invokeAttr: BindingFlags.Static | BindingFlags.SetProperty | BindingFlags.NonPublic |
+                                            BindingFlags.Public,
+                                binder: null,
+                                index: null,
+                                culture: CultureInfo.InvariantCulture
+                            );
+                        }
+                        else
+                        {
+                            Debug.LogError("SyncGripMoveGUIPlugin Error: IsVisble property not found.");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError("SyncGripMoveGUIPlugin Error: IMGUIQuad type not found.");
+                    }
                 }
                 else
                 {
-                    Logger.LogInfo("Find CM3D2.GripMovePlugin.Plugin.GUIQuad.switchVisibility method success.");
+                    Debug.LogError("SyncGripMoveGUIPlugin Error: GripMovePlugin assembly not found.");
                 }
             }
-            else
+            catch (System.Exception ex)
             {
-                Logger.LogError("CM3D2.GripMovePlugin.Plugin.GUIQuad type not found.");
-            }
-        }
-
-        [HarmonyPatch(typeof(OvrTablet), "SetVisible")]
-        public static class OvrTabletSetVisiblePatch
-        {
-            static void Postfix(OvrTablet __instance, bool f_bVisible)
-            {
-                if (!isVRMode) return;
-                // Called after the SetVisible method executes
-                if (_toggleGUIVisibleMethod != null)
-                {
-                    try
-                    {
-                        _toggleGUIVisibleMethod.Invoke(null, new object[] { f_bVisible });
-                        Logger.LogInfo(
-                            $"Old GUI visibility toggled due to OvrTablet visibility change. New visibility: {f_bVisible}");
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.LogError($"Error invoking switchVisibility: {ex.Message}");
-                    }
-                }
+                Debug.LogError("SyncGripMoveGUIPlugin Error: " + ex);
             }
         }
     }
