cf15a6b13433652344b39a34312e4ceea0bf72ca
InoryS <101243352+InoryS@users.noreply.github.com>
2024-11-06 19:29:24 +0800

    1.1.0.0 fix every thing

--- COM3D2.SyncGripMoveGUI.Plugin/Plugin.cs
+++ COM3D2.SyncGripMoveGUI.Plugin/Plugin.cs
@@ -6,18 +6,20 @@ using UnityEngine;
 using System.Linq;
 using System.Globalization;
 
+[assembly: AssemblyVersion("1.1.0.0")]
 namespace COM3D2.SyncGripMoveGUI.Plugin
 {
-    [BepInPlugin("COM3D2.SyncGripMoveGUI.Plugin", "COM3D2.SyncGripMoveGUI.Plugin", "1.0.9.0")]
+    [BepInPlugin("COM3D2.SyncGripMoveGUI.Plugin", "COM3D2.SyncGripMoveGUI.Plugin", "1.1.0.0")]
     public class SyncGripMoveGUI : BaseUnityPlugin
     {
-        public static bool isOfficalTableVisible = false;
+        public static bool isOfficialTableVisible = false;
         public static bool isDirectModeActive = false;
 
         private bool previousIsDirectModeActive = false;
+
         private Transform leftHandAnchor;
         private Transform rightHandAnchor;
-        private static PropertyInfo isVisbleProperty = null;
+        private static PropertyInfo isVisbleProperty;
 
         private ViveController leftViveController;
         private ViveController rightViveController;
@@ -36,11 +38,61 @@ namespace COM3D2.SyncGripMoveGUI.Plugin
             harmony.PatchAll();
             CacheReflectionData();
 
-            // 获取左手和右手控制器的 Transform
             leftHandAnchor = GameMain.Instance.OvrMgr.GetVRControllerTransform(true);
             rightHandAnchor = GameMain.Instance.OvrMgr.GetVRControllerTransform(false);
 
-            // 缓存控制器组件
+            InitializeControllers();
+        }
+
+        void Update()
+        {
+            isDirectModeActive = IsDirectMode();
+            if (isDirectModeActive != previousIsDirectModeActive)
+            {
+                previousIsDirectModeActive = isDirectModeActive;
+
+                if (isOfficialTableVisible && isDirectModeActive)
+                {
+                    ChangeOldGUIVisible(true);
+                }
+                else
+                {
+                    ChangeOldGUIVisible(false);
+                }
+
+                // Debug.Log("Direct Mode Change to " + isDirectModeActive);
+            }
+        }
+
+        private void CacheReflectionData()
+        {
+            try
+            {
+                var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+                var gripMoveAssembly =
+                    assemblies.FirstOrDefault(a => a.GetType("CM3D2.GripMovePlugin.Plugin.IMGUIQuad") != null);
+                if (gripMoveAssembly == null)
+                {
+                    Debug.LogError("SyncGripMoveGUIPlugin Error: GripMovePlugin assembly not found.");
+                    return;
+                }
+
+                var imguiQuadType = gripMoveAssembly.GetType("CM3D2.GripMovePlugin.Plugin.IMGUIQuad");
+                isVisbleProperty = imguiQuadType?.GetProperty("IsVisble",
+                    BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+                if (isVisbleProperty == null)
+                {
+                    Debug.LogError("SyncGripMoveGUIPlugin Error: IMGUIQuad IsVisble Property not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("SyncGripMoveGUIPlugin: CacheReflectionData Error : " + ex);
+            }
+        }
+
+        private void InitializeControllers()
+        {
             if (GameMain.Instance.VRFamily == GameMain.VRFamilyType.Oculus)
             {
                 leftOvrController = leftHandAnchor?.gameObject.GetComponent<OvrController>();
@@ -53,89 +105,47 @@ namespace COM3D2.SyncGripMoveGUI.Plugin
             }
         }
 
-        void Update()
-        {
-            isDirectModeActive = IsDirectMode();
-            if (isDirectModeActive != previousIsDirectModeActive)
-            {
-                previousIsDirectModeActive = isDirectModeActive;
-                if (isOfficalTableVisible && isDirectModeActive)
-                {
-                    ChangeOldGUIVisible(true);
-                }
-                Debug.Log("Direct Mode 状态已更改为: " + isDirectModeActive);
-            }
-        }
-
-        private static void CacheReflectionData()
-        {
-            try
-            {
-                var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-                var gripMoveAssembly =
-                    assemblies.FirstOrDefault(a => a.GetType("CM3D2.GripMovePlugin.Plugin.IMGUIQuad") != null);
-                if (gripMoveAssembly != null)
-                {
-                    var imguiQuadType = gripMoveAssembly.GetType("CM3D2.GripMovePlugin.Plugin.IMGUIQuad");
-                    isVisbleProperty = imguiQuadType?.GetProperty("IsVisble",
-                        BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
-                    if (isVisbleProperty == null)
-                    {
-                        Debug.LogError("SyncGripMoveGUIPlugin Error: IsVisble Property not found.");
-                    }
-                }
-                else
-                {
-                    Debug.LogError("SyncGripMoveGUIPlugin Error: GripMovePlugin assembly not found.");
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError("SyncGripMoveGUIPlugin: CacheReflectionData Error : " + ex);
-            }
-        }
-
         private bool IsDirectMode()
         {
-            bool isDirect = false;
-            if (GameMain.Instance.VRFamily == GameMain.VRFamilyType.Oculus)
+            // Check if in Direct mode of GripMove
+            return GameMain.Instance.VRFamily switch
             {
-                if (!leftOvrController)
-                {
-                    leftOvrController = leftHandAnchor?.gameObject.GetComponent<OvrController>();
-                }
+                GameMain.VRFamilyType.Oculus => CheckOvrControllers(),
+                GameMain.VRFamilyType.HTC => CheckViveControllers(),
+                _ => false
+            };
+        }
 
-                if (!rightOvrController)
-                {
-                    rightOvrController = rightHandAnchor?.gameObject.GetComponent<OvrController>();
-                }
-
-                if ((!leftOvrController || !leftOvrController.enabled) ||
-                    (!rightOvrController || !rightOvrController.enabled))
-                {
-                    isDirect = true;
-                }
-            }
-            else if (GameMain.Instance.VRFamily == GameMain.VRFamilyType.HTC)
+        private bool CheckOvrControllers()
+        {
+            if (!leftOvrController)
             {
-                if (!leftViveController)
-                {
-                    leftViveController = leftHandAnchor?.gameObject.GetComponent<ViveController>();
-                }
-
-                if (!rightViveController)
-                {
-                    rightViveController = rightHandAnchor?.gameObject.GetComponent<ViveController>();
-                }
-
-                if ((!leftViveController || !leftViveController.enabled) ||
-                    (!rightViveController || !rightViveController.enabled))
-                {
-                    isDirect = true;
-                }
+                leftOvrController = leftHandAnchor?.gameObject.GetComponent<OvrController>();
             }
 
-            return isDirect;
+            if (!rightOvrController)
+            {
+                rightOvrController = rightHandAnchor?.gameObject.GetComponent<OvrController>();
+            }
+
+            return (!leftOvrController || !leftOvrController.enabled) ||
+                   (!rightOvrController || !rightOvrController.enabled);
+        }
+
+        private bool CheckViveControllers()
+        {
+            if (!leftViveController)
+            {
+                leftViveController = leftHandAnchor?.gameObject.GetComponent<ViveController>();
+            }
+
+            if (!rightViveController)
+            {
+                rightViveController = rightHandAnchor?.gameObject.GetComponent<ViveController>();
+            }
+
+            return (!leftViveController || !leftViveController.enabled) ||
+                   (!rightViveController || !rightViveController.enabled);
         }
 
         public static void ChangeOldGUIVisible(bool visible)
@@ -144,18 +154,15 @@ namespace COM3D2.SyncGripMoveGUI.Plugin
             {
                 if (isVisbleProperty == null)
                 {
-                    CacheReflectionData();
+                    // Ensure isVisible Property is up to date
+                    var instance = new SyncGripMoveGUI();
+                    instance.CacheReflectionData();
                 }
 
-                isVisbleProperty?.SetValue(
-                    obj: null,
-                    value: visible,
-                    invokeAttr: BindingFlags.Static | BindingFlags.SetProperty | BindingFlags.NonPublic |
-                                BindingFlags.Public,
-                    binder: null,
-                    index: null,
-                    culture: CultureInfo.InvariantCulture
-                );
+                // This property controls whether the OldGU (IMGUI) of GripMove is visible.
+                isVisbleProperty?.SetValue(null, visible,
+                    BindingFlags.Static | BindingFlags.SetProperty | BindingFlags.NonPublic | BindingFlags.Public, null,
+                    null, CultureInfo.InvariantCulture);
             }
             catch (Exception ex)
             {
@@ -168,7 +175,8 @@ namespace COM3D2.SyncGripMoveGUI.Plugin
         {
             public static void Postfix(bool f_bShow)
             {
-                SyncGripMoveGUI.isOfficalTableVisible = f_bShow;
+                // f_bShow Indicates whether the official tablet GUI is visible
+                SyncGripMoveGUI.isOfficialTableVisible = f_bShow;
                 if (!f_bShow)
                 {
                     SyncGripMoveGUI.ChangeOldGUIVisible(f_bShow);
